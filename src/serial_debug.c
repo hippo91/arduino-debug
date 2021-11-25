97dbc58ee70e36a35c6bd632aad19ab391fe883f
hippo91 <guillaume.peillex@gmail.com>
2021-11-25 18:53:05 +0100

    Initial import

---
+++ src/serial_debug.c
@@ -0,0 +1,54 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "serial_debug.h"
+
+void USART_Init(unsigned int ubrr)
+{
+  /*Set baud rate */
+  UBRR0H = (unsigned char)(ubrr>>8);
+  UBRR0L = (unsigned char)ubrr;
+  UCSR0B = 0x00;
+  UCSR0C = 0x00;
+  /* Enable transmitter */
+  UCSR0B = (0<<RXEN0)|(1<<TXEN0);
+  /* Set frame format: 8data, 1stop bit */
+  UCSR0C = (0<<USBS0)|(0<<UCSZ02)|(1<<UCSZ01)|(1<<UCSZ00);
+}
+
+void USART0SendByte(const unsigned char byte)
+{
+    //wait while previous byte is completed
+    while(!(UCSR0A&(1<<UDRE0))){};
+    // Transmit data
+    UDR0 = byte;
+}
+
+void USART0SendData(const unsigned char* data)
+{
+  while(*data != 0) {
+    USART0SendByte(*data);
+    data++;
+  }
+  USART0SendByte('\n');
+  USART0SendByte('\r');
+}
+
+void DumpRegister(const uint8_t reg, uint8_t* buffer)
+{
+  for (uint8_t index=0; index < REGISTER_SIZE; ++index) {
+    if (reg & (1<<index)) {
+      buffer[REGISTER_SIZE-1-index] = '1';
+    } else {
+      buffer[REGISTER_SIZE-1-index] = '0';
+    }
+  }
+  buffer[REGISTER_SIZE] = 0;
+}
+
+void USART0DumpRegister(const uint8_t reg)
+{
+  uint8_t reg_brep[REGISTER_SIZE+1]; // +1 For null terminated string
+  DumpRegister(reg, reg_brep);
+  USART0SendData(reg_brep);
+}
---
+++ src/serial_debug.h
@@ -0,0 +1,67 @@
+#ifndef SERIAL_DEBUG_H
+#define SERIAL_DEBUG_H
+
+#define REGISTER_SIZE 8
+
+/**
+ * @brief Compute the value of the USART Baud Rate Register
+ * 
+ * @param FREQ : cpu frequency
+ * @param BAUDRATE : transmission rate
+ */
+#define COMPUTE_UBRR(FREQ, BAUDRATE) (((FREQ / (16UL * BAUDRATE)) - 1))
+
+/*--------------------------------------------------------*/
+/**
+ * @brief Initialize the USART communication by
+ *        setting the baud rate inside the UBRR register
+ * 
+ * @param ubrr: the value to put in the UBRR register
+ * @note: the value to put in the UBRR register is linked 
+ *        to the frequency and the baud rate thanks to the
+ *        following formula:
+ *         \f$((F_{CPU} / (16UL * BAUD)) - 1)\f$ 
+ * @note: the frame format is set to 8 data bits and 1 stop bit
+ */
+/*--------------------------------------------------------*/
+void USART_Init(unsigned int ubrr);
+
+/*--------------------------------------------------------*/
+/**
+ * @brief Send the byte in argument through the serial port 
+ * 
+ * @param byte : data to transmit
+ */
+/*--------------------------------------------------------*/
+void USART0SendByte(const unsigned char byte);
+
+/*--------------------------------------------------------*/
+/**
+ * @brief Send the data in argument through the serial port
+ * 
+ * @param data : data to transmit
+ */
+/*--------------------------------------------------------*/
+void USART0SendData(const unsigned char* data);
+
+/*--------------------------------------------------------*/
+/**
+ * @brief Dumps the binary representation of the register into buffer
+ * 
+ * @param reg : register to be represented as a binary string
+ * @param buffer : string that will hold the binary representation
+ */
+/*--------------------------------------------------------*/
+void DumpRegister(const uint8_t reg, uint8_t* buffer);
+
+/*--------------------------------------------------------*/
+/**
+ * @brief Send the binary representation of the register
+ *        through serial output
+ * 
+ * @param reg : register to be represented as a binary string
+ */
+/*--------------------------------------------------------*/
+void USART0DumpRegister(const uint8_t reg);
+
+#endif  // SERIAL_DEBUG_H
